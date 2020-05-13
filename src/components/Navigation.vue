abc858cb09bb4d1419341ce5f54eecf3b2a824e1
syzsi <syzsi@elesy.ru>
2020-05-13 07:36:13 +0700

    add store

---
+++ src/components/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Navbar w/ text</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+        <span class="navbar-text">
+          {{ getCurrentUser }}
+        </span>
+      </div>
+    </nav>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+
+export default {
+  name: 'App',
+  computed: mapGetters(["getCurrentUser"]),
+}
+</script>
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import users from './modules/users'
+import axios from 'axios'
+
+Vue.use(Vuex)
+Vue.use(axios)
+
+export default new Vuex.Store({
+    modules: {
+        users
+    }
+})
