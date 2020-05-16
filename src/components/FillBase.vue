5ecabac29a06251855ab593bc3cf1969f49e271b
syzsi <syzsi@elesy.ru>
2020-05-16 11:50:35 +0700

    Справочники

--- src/components/FillBase.vue
+++ src/components/FillBase.vue
@@ -1,77 +1,189 @@
 <template>
-    <div>
-    <div class="container align-middle col-sm-6 col-md-4 col-lg-4 col-xl-4">
-      <form  @submit="send_skill_name">
-        <div class="form-group">
-          <label for="skill_name_desc">Описание навыка</label>
-          <input
-            class="form-control"
-            id="skill_name_desc"
-            v-model="skillNameDesc"
-            required
-          />
-        </div>
-        <div class="container-fluid">
-            <label for="inputState">State</label>
-            <select id="inputState" class="form-control" v-model="skillNameArea">
-                <option selected>Верхний уровень</option>
-                <option>Средний уровень</option>
-                <option>Конструкторская документаця</option>
-                <option>Эксплуатационная документаця</option>
+  <div class="container-fluid" style="margin-top : 30px">
+    <h4>Справочники</h4>
+    <div class="row justify-content-md-center">
+      <div class="skill-desc-form col-sm">
+        <h6>Навык</h6>
+        <form @submit="send_skill_desc">
+          <div class="align-middle input-field">
+            <select
+              id="skill_desc_area"
+              class="form-control-sm input-field"
+              v-model="skillDescArea"
+            >
+              <option value="VU" selected>Верхний уровень</option>
+              <option value="SU">Средний уровень</option>
+              <option value="KD">Конструкторская документаця</option>
+              <option value="ED">Эксплуатационная документаця</option>
             </select>
-        </div>
-        <div class="form-group">
-          <label for="skill_name_base">Базовое ПО</label>
-          <input
-            class="form-control"
-            id="skill_name_base"
-            v-model="skillNameBase"
-            required
-          />
-        </div>
-        
-        <button type="submit" class="btn btn-dark">Сделать мир лучше!</button>
-      </form>
+          </div>
+
+          <div class="form-group input-field">
+            <input
+              placeholder="Описание навыка"
+              class="form-control-sm input-field "
+              id="skill_desc_desc"
+              v-model="skillDescDesc"
+              required
+            />
+          </div>
+
+          <div class="form-group input-field">
+            <input
+              placeholder="Базовое ПО"
+              class="form-control-sm input-field "
+              id="skill_desc_base"
+              v-model="skillDescBase"
+              required
+            />
+          </div>
+          <button type="submit" class="btn btn-dark input-field">
+            Добавить
+          </button>
+        </form>
+      </div>
+
+      <div class="skill-desc-form col-sm">
+        <h6>Сертификаты, протколы, удостоверения</h6>
+        <form @submit="send_cert_desc">
+          <div class="form-group input-field">
+            <input
+              placeholder="Описание удостоверения/протокола"
+              class="form-control-sm input-field "
+              id="skill_cert_desc"
+              v-model="skillCertDesc"
+              required
+            />
+          </div>
+          <button type="submit" class="btn btn-dark input-field">
+            Добавить
+          </button>
+        </form>
+      </div>
+
+      <div class="skill-desc-form col-sm">
+        <h6>Группы/отделы</h6>
+        <form @submit="send_group_name">
+          <div class="form-group input-field">
+            <input
+              placeholder="Название группы/отдела"
+              class="form-control-sm input-field "
+              id="group_name"
+              v-model="groupName"
+              required
+            />
+          </div>
+          <button type="submit" class="btn btn-dark input-field">
+            Добавить
+          </button>
+        </form>
+      </div>
+
+    </div>
+    <div class="row justify-content-md-center">
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            payload : {},
-            skillNameDesc : "",
-            skillNameArea : "",
-            skillNameBase : ""
-        }
-    },
-    methods: {
-    send_skill_name(evt) {      
-        evt.preventDefault();
-        this.response = "";
-        const path = 'http://localhost:5000/api/skills/addname';
-      
-        this.payload = {
-            "description" : this.skillNameDesc,
-            "area" : this.skillNameArea,
-            "baseSoftware" : this.skillNameBase
-        },
-        this.axios.post(path, this.payload)
-          .then((res) => {
+  data() {
+    return {
+      payload: {},
+      CertDescDesc: "",
+      skillDescArea: "",
+      skillDescDesc: "",
+      skillDescBase: "",
+      skillCertDesc: "",
+      groupName: ""
+    };
+  },
+  methods: {
+    send_skill_desc(evt) {
+      evt.preventDefault();
+      this.response = "";
+      const path = "http://10.99.0.103:5000/api/skills/addname";
+
+      (this.payload = {
+        description: this.skillDescDesc,
+        area: this.skillDescArea,
+        baseSoftware: this.skillDescBase
+      }),
+        this.axios
+          .post(path, this.payload)
+          .then(res => {
             this.response = res.data;
             console.log(this.response.message);
             if (this.response.status === "success") {
               console.log(res);
-              this.skillNameDesc = "",
-              this.skillNameArea = "",
-              this.skillNameBase = ""
+              this.skillDescDesc = "";
             }
           })
-          .catch((error) => {
+          .catch(error => {
             console.error(error);
+          });
+    },
+    send_cert_desc(evt) {
+      evt.preventDefault();
+      this.response = "";
+      const path = "http://10.99.0.103:5000/api/certs/addname";
+
+      (this.payload = {
+        description: this.skillCertDesc
+      }),
+        this.axios
+          .post(path, this.payload)
+          .then(res => {
+            this.response = res.data;
+            console.log(this.response.message);
+            if (this.response.status === "success") {
+              console.log(res);
+              this.skillCertDesc = "";
+            }
           })
+          .catch(error => {
+            console.error(error);
+          });
+    },
+    send_group_name(evt) {
+      evt.preventDefault();
+      this.response = "";
+      const path = "http://10.99.0.103:5000/user/addgroup";
+
+      (this.payload = {
+        groupname: this.groupName
+      }),
+        this.axios
+          .post(path, this.payload)
+          .then(res => {
+            this.response = res.data;
+            console.log(this.response.message);
+            if (this.response.status === "success") {
+              console.log(res);
+              this.groupName = "";
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
     }
-  },
-}
+  }
+};
 </script>
+
+<style scoped>
+.skill-desc-form {
+  margin: 10px;
+  border: 1px;
+  border-style: solid;
+  border-radius: 3px;
+  border-color: #aaa;
+  padding: 10px;
+  max-width: 500px;
+}
+.input-field {
+  margin: 2px;
+  width: 100%;
+  font-size: 0.7rem;
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,20 +1,20 @@
 <template>
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar w/ text</a>
+      <a class="navbar-brand" href="#">ЭлеСи-Про</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="#" v-on:click="goToAllProjects">Проекты<span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Разработчики</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#" v-on:click="goToFillBase">Справочники</a>
           </li>
         </ul>
         <span class="navbar-text user-name" v-on:click="goToLogin">
@@ -34,6 +34,12 @@ export default {
   methods: {
       goToLogin(){
           this.$router.push('/user/login');
+      },
+      goToFillBase(){
+          this.$router.push('/fillbase');
+      },
+      goToAllProjects(){
+          this.$router.push('/projects/all');
       },
   }
 }
